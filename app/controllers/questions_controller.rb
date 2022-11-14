ff8ca4183b865f91eb7b974be720197bc05069a8
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-14 13:17:35 -0300

    add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,15 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question].downcase
-    @answer = '...'
-    if @question == 'i am going to work'
+    if @question == ''
+      @answer = '...'
+    elsif @question == 'i am going to work'
       @answer = 'Great!'
     elsif @question[-1] == '?'
       @answer = ' Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
+      @answer
     end
     @answer
   end
